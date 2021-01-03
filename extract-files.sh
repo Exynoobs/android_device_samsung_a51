cad5da028179de0d5c313493f56d288a833843f1
Tim Zimmermann <tim@linux4.de>
2021-01-03 13:03:59 +0100

    a51: use patchelf instead of TARGET_LD_SHIM_LIBS
    Change-Id: I1ee5b574dea7548e1c81a33af6c5f9f22fd89841

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,17 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/vndk/libstagefright_omx_utils.so)
+            "${PATCHELF}" --add-needed libshim_stagefright_foundation.so "${2}"
+            ;;
+        vendor/lib/libsensorlistener.so)
+            "${PATCHELF}" --add-needed libshim_sensorndkbridge.so "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
